6c11acd5c0399c8a220537b1911e3f8b5509eb36
prabhattechnohunk <prabhatproject1@gmail.com>
2026-06-09 06:22:07 +0530

    first commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //create container and beans inside it
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+//Hey container--give me student1 bean
+        Student student01 = (Student) context.getBean("student1");
+        System.out.println(student01);
+        //Hey container--give me student2 bean
+        Student student02 = (Student) context.getBean("student2");
+        System.out.println(student02);
+
+
+
+    }
+}
