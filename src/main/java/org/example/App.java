84e2967eb38bda1209601143e0395b10c6ef5dcb
prabhattechnohunk <prabhatproject1@gmail.com>
2026-06-09 06:26:56 +0530

    removing old comment

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,10 +3,7 @@ package org.example;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Hello world!
- *
- */
+//main class
 public class App 
 {
     public static void main( String[] args )
